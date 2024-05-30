9d39969660cf34d1c2eed1a63c48c556273413fd
khaulinD <dimakhaulin@gmail.com>
2024-05-30 20:33:43 +0300

    1234

--- auth_system/db-init.sh
+++ auth_system/db-init.sh
@@ -1,12 +1,12 @@
-echo "Waiting for DB to be reachable..."
-until psql --host=$DATABASE_HOST --port=$DATABASE_PORT --username=$DATABASE_USER --dbname=$DATABASE_NAME -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
-# Run migrations
-echo "Running migrations..."
-alembic upgrade head
+#echo "Waiting for DB to be reachable..."
+#until psql --host=$DATABASE_HOST --port=$DATABASE_PORT --username=$DATABASE_USER --dbname=$DATABASE_NAME -c '\l'; do
+#  >&2 echo "Postgres is unavailable - sleeping"
+#  sleep 1
+#done
+#
+## Run migrations
+#echo "Running migrations..."
+#alembic upgrade head
 
 # Start the main process
 echo "Starting server..."
