09ef7d99a17b635171b735e37c377aaa11a4799a
khaulinD <dimakhaulin@gmail.com>
2024-05-30 20:28:42 +0300

    upgrade sh file

--- auth_system/db-init.sh
+++ auth_system/db-init.sh
@@ -1,5 +1,13 @@
-#python3 -m venv venv
-#source venv/bin/activate
-#pip3 install -r requirements.txt
+echo "Waiting for DB to be reachable..."
+until psql --host=$DATABASE_HOST --port=$DATABASE_PORT --username=$DATABASE_USER --dbname=$DATABASE_NAME -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
 
-python -m db.create_tables
+# Run migrations
+echo "Running migrations..."
+alembic upgrade head
+
+# Start the main process
+echo "Starting server..."
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
